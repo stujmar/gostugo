5b1e785c54a4eb79155ff558f66bc668811ff383
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-01-21 20:04:05 -0600

    brought in json data for vue quiz app

---
+++ data.js
@@ -0,0 +1,35 @@
+data = [{
+  "id": 0,
+  "question": "This directive is used to conditionally render a block. The block will only be rendered 'if' the directive’s expression returns a truthy value.",
+  "answer": "v-if "  
+},
+{
+    "id": 1,
+    "question": "This is v-if's little brother, he renders conditionally with higher upfront cost but is quicker to toggle during run time.",
+    "answer": "v-show"  
+  },
+  {
+    "id": 2,
+    "question": "Directive to render a list of items based on an array.",
+    "answer": "v-for"  
+  },
+  {
+    "id": 3,
+    "question": "This alternative to Vue CLI install is for prototyping or learning purposes, or for quickly loading view in a portion of an existing website.",
+    "answer": "Vue CDN"  
+  },
+  {
+    "id": 4,
+    "question": "This installation type of Vue will provide you with helpful tools like webpack.",
+    "answer": "Vue CLI"  
+  },
+  {
+    "id": 5,
+    "question": "This directive binds data to a element especially useful when passing props to a child.",
+    "answer": "v-bind"  
+  },
+  {
+    "id": 6,
+    "question": "We can use this directive to listen to DOM events and run some JavaScript when they’re triggered.",
+    "answer": "v-on"  
+  }]
