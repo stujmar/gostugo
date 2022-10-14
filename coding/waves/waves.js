4b615f81a594347d5fae940482d367edab4e2576
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-10-13 22:14:19 -0500

    [sm] made it draw a line instead of dots

--- coding/waves/waves.js
+++ coding/waves/waves.js
@@ -1,5 +1,26 @@
 console.log("Waves");
+class Line {
+    constructor(x1, y1, x2, y2) {
+        this.start = {x: x1, y: y1};
+        this.end = {x: x2, y: y2};
+    }
+    draw() {
+      // save the current drawing style
+      push();
+        stroke(255,0,0);
+        strokeWeight(4);
+        if(abs(this.start.x - this.end.x) < 3 && abs(this.start.y - this.end.y) < 50) {
+        line(this.start.x, this.start.y, this.end.x, this.end.y);
+        }
+      pop();
+    }
+    update(_x,_y) {
+      this.start = this.end;
+      this.end = createVector(_x,_y);
+}
+}
 
+let myLine;
 let width = 800;
 let height = 800;
 let x = 0;
@@ -7,19 +28,20 @@ let y = 0;
 
 
 function setup() {
+  myLine = new Line(0,200,0,200);
   createCanvas(width, height);
 }
 
 function draw() {
-  fill(0, 0, 0);
-  noStroke();
-  ellipse(x,(Math.sin(y) * 150)+ 200,5,5);
-  console.log(Math.sin(x));
+  myLine.update(x,(Math.sin(y) * 150)+ 200);
   if (x < width) {
+    myLine.draw();
+    // noStroke();
+    // fill(0, 0, 0);
+    // ellipse(x,(Math.sin(y) * 150)+ 200,6,6);
     x+=2;
   } else {
     x = 0;
   }
   y = y + .05;
-  
 }
